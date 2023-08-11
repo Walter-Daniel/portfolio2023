282e734b37650618a9066ea553895cd32d01be23
Walter <walterdcarrizo.19@gmail.com>
2023-08-11 19:44:33 -0300

    Limpieza de código

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -11,14 +11,14 @@ export const Contact = () => {
         <Typography variant='h3' color={theme.palette.primary.main} sx={{ textTransform:'uppercase', fontSize:'1.5rem', fontWeight:'bold', paddingBottom:'1rem'}}>
           contacto
         </Typography>
-        <Typography fontWeight='bold' marginBottom='0.5rem'>Puedes contactarme a través de mi linkedin o email  <ArrowDownwardIcon /></Typography>
+        <Typography fontWeight='bold' marginBottom='0.5rem'>Puedes contactarme a través de mi email o linkedin  <ArrowDownwardIcon /></Typography>
         <Grid container direction='row' width='100%' justifyContent='space-around'>
           <Grid item xs={12} md={6} display='flex' marginBottom='0.5rem'>
             <AlternateEmailIcon sx={{ backgroundColor:'black', borderRadius:'50%', padding:'5px', color:'white', marginRight:'0.5rem' }}/> 
           <Typography> Email: walterdcarrizo.19@gmail.com</Typography>
           </Grid>
           <Grid item xs={12} md={6}>
-            <Button variant='contained' color='secondary' startIcon={<LinkedInIcon />} href='https://www.linkedin.com/in/walter-daniel-carrizo/' target='_blank'>Linkedin</Button>
+            <Button variant='contained' color='secondary' startIcon={<LinkedInIcon />} href='https://www.linkedin.com/in/walter-daniel-carrizo/' target='_blank' sx={{ width:{xs:'100%', md:'auto'} }}>Linkedin</Button>
           </Grid>
           
         </Grid>
