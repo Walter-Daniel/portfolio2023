0cfe65fd075688a5ef28b0594cd2b17f632deafe
Walter <walterdcarrizo.19@gmail.com>
2023-08-12 17:33:52 -0300

    Se agregó margin en breakpoint xs

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
   const theme = useTheme()
 
   return (
-    <Grid container sx={{ minHeight: '100vh' }} id='about' display='flex'alignItems='center'>
+    <Grid container sx={{ minHeight: '100vh', margin:{xs:'6rem 0', sm:'0'} }} id='about' display='flex'alignItems='center'>
       <Grid item xs={12} md={6}>
         <figure className='about-img'>
           <img src={aboutIMG} alt="Hombre trabajando con una computadora" />
@@ -20,7 +20,7 @@ export const About = () => {
       </Typography>
       <Typography>Cuento con experiencia en proyectos grupales e individuales, soy proactivo, me gusta investigar y poner en práctica nuevas formas de optimizar mi código y mejorar la experiencia del usuario.</Typography>
       <Typography> Mis mayores fortalezas son mi paciencia, mi empatía y mi empeño en aprender. Soy amante de la tecnología, fotografía y de la naturaleza.</Typography>
-      <Button variant='contained' color='primary' style={{ marginTop:'0.5rem', textTransform:'capitalize' }}>ver currículo</Button>
+      <Button variant='contained' color='primary' sx={{ marginTop:'0.5rem', textTransform:'capitalize', width:{xs:'100%', sm:'auto'} }}>ver currículo</Button>
 
       </Grid>
 
