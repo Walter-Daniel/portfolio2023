7efb3ea279e4514a0e25458c4c9d8f18ed49ee3c
Walter <walterdcarrizo.19@gmail.com>
2024-09-04 18:28:34 -0300

    Se actualizaron los links en proyectos

--- src/helpers/portfolioInfo.js
+++ src/helpers/portfolioInfo.js
@@ -9,7 +9,7 @@ export const portfolioInfo = [
         img: purpleShopIMG,
         description: 'Purple Shop es un e-commerce de venta de indumentaria donde puedes seleccionar tus prendas, elegir el tamaño y la cantidad, añadir una dirección de entrega y pagar de forma segura a través de una pasarela de pago integrada con PayPal. Dispone de un panel de administración exclusivo para el administrador, quien puede crear, editar y eliminar productos, así como supervisar el estado de las órdenes y la gestión de usuarios',
         tecnology:'Next.js, TypeScript, Auth.js',
-        path: 'https://astrowiki-purpledev.vercel.app/',
+        path: 'https://purple-shop-theta.vercel.app/',
         github: 'https://github.com/Walter-Daniel/purple-shop',
         githubBack: null
     },
@@ -18,7 +18,7 @@ export const portfolioInfo = [
         img: astrowikiIMG,
         description: 'AstroWiki es un sitio web educativo donde prodrás consultar información sobre asteroides cercanos a la Tierra y exoplanetas. Este proyecto frontend consume la API de la NASA e incluye una página de inicio, contacto, formularios, y un componente hecho con three.js que compara el radio de la Tierra con el exoplaneta más grande.',
         tecnology:'HTML, TailwindCSS, TypeScript, React, Three.js',
-        path: 'https://purple-shop-theta.vercel.app/',
+        path: 'https://astrowiki-purpledev.vercel.app/',
         github: 'https://github.com/Walter-Daniel/curso-next/tree/main/clase-1',
         githubBack: null
     },
@@ -27,7 +27,7 @@ export const portfolioInfo = [
         img: journalIMG,
         description: 'Journal App es una aplicación fullstack que te permite crear, editar y eliminar notas, así como añadir imágenes. Todas las rutas están protegidas, por lo que es necesario registrarse en la aplicación y luego iniciar sesión para acceder a sus funcionalidades.',
         tecnology: 'HTML, CSS, React-Redux-Toolkit, MIU, NodeJs, Express, MongoDB, Cloudinary',
-        path: 'https://purple-journal-app.netlify.app/',
+        path: 'https://journal-project.vercel.app/auth/login',
         github: 'https://github.com/Walter-Daniel/JournalProyect',
         githubBack: 'https://github.com/Walter-Daniel/JournalProyect-Backend'
     },
